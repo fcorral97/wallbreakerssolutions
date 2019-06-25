a67c256c0bdf8190fb25c506773ac18293141489
kr0wl <fcorral@smu.edu>
2019-06-25 19:17:45 -0400

    Update ReverseString.cpp

--- Week_1/ReverseString.cpp
+++ Week_1/ReverseString.cpp
@@ -4,9 +4,14 @@ Wall Breakers
 Cohort 3
 Reverse String
 */
+
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(), s.end());
+        for(int i = 0; i < s.size()/2; i++){
+            int tmp = s[(s.size()-1)-i];
+            s[(s.size()-1)-i] = s[i];
+            s[i]= tmp;
+        }
     }
 };
