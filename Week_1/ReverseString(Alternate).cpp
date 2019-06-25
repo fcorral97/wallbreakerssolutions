ee0654025d37769c412ed0aa414ef18f843db762
kr0wl <fcorral@smu.edu>
2019-06-25 19:17:28 -0400

    Create ReverseString(Alternate).cpp

---
+++ Week_1/ReverseString(Alternate).cpp
@@ -0,0 +1,16 @@
+/*
+Fernando Corral
+Wall Breakers
+Cohort 3
+Reverse String Alternate Solution
+*/
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for(int i = 0; i < s.size()/2; i++){
+            int tmp = s[(s.size()-1)-i];
+            s[(s.size()-1)-i] = s[i];
+            s[i]= tmp;
+        }
+    }
+};
