71d55868d1e28f929c3e16764eb686e270badc95
kr0wl <fcorral@smu.edu>
2019-07-02 18:24:06 -0400

    Update NumJewelsInStone.cpp

--- Week_2/NumJewelsInStone.cpp
+++ Week_2/NumJewelsInStone.cpp
@@ -2,29 +2,21 @@
 Fernando Corral
 Wall Breakers
 Cohort 3
-Detect Capital Use
+Num Jewels In Stones
 */
 class Solution {
 public:
-    bool detectCapitalUse(string word) {
-        bool all = true;
-        for(int n = word.size()-1; n >= 0; n--){
-            if(word.size() == 1){
-                return true;
-            }
-            if(n == 0 & int(word[n]) > 96 & all ){
-                return false;
-            }
-            if(n != 0 & int(word[n]) < 96 & !all){
-                return false;
-            }
-            if(int(word[n]) > 96 & all){
-                if(n < (word.size()-1)){
-                    return false;
-                }
-                all = false;
+    int numJewelsInStones(string J, string S) {
+        int solution = 0;
+        map<char, bool> characters;
+        for(char i: J){
+            characters[i] = true;
+        }
+        for(char i: S){
+            if(characters[i]){
+                solution++;
             }
         }
-        return true;
+        return solution;
     }
 };
