d21a2d47638a93c1499cd29f574069406149165a
kr0wl <fcorral@smu.edu>
2019-07-07 14:47:24 -0400

    Create MyHashMap.cpp

---
+++ Week_2/MyHashMap.cpp
@@ -0,0 +1,39 @@
+/*
+Fernando Corral
+Wallbreakers
+Cohort 3
+My Hash Map
+*/
+class MyHashMap {
+public:
+    int data[1000000];
+    /** Initialize your data structure here. */
+    MyHashMap() {
+        for(int x = 0; x < 1000000; x++){
+            data[x] = -1;
+        }
+    }
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        data[key] = value;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        return data[key];
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        data[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
